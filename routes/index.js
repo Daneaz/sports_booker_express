3ddd6300cdbcd57e8a8c849648c438cedb698413
Eugene <eugenewwj@gmail.com>
2023-07-02 16:06:18 +0800

    BUG FIX

--- routes/index.js
+++ routes/index.js
@@ -143,10 +143,10 @@ async function bookingSlot(req, res = null) {
 
         logger.info(`Obtained ruleId: ${ruleId}`)
 
-        let expiredTime = moment().add(30, 'seconds')
+        let expiredTime = moment().add(60, 'seconds')
         let status = 499;
         let counter = 1;
-        while (status >= 400 && moment.now() < expiredTime.valueOf()) {
+        while (status !== 200 && moment.now() < expiredTime.valueOf()) {
             logger.info(`Trying ${counter}`)
             status = await bookSlot(cookies, sessionId, ruleId, res);
             await delay(500);
